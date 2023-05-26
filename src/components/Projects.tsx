ace3a00c68c6401546861134dfa6aa55cc4afab3
d.saez <dssh27@gmail.com>
2023-05-26 09:00:03 -0400

    cards project modificado

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,46 +4,24 @@ import { pathImages } from "@/helpers";
 export const Projects = () => {
     return (
         <section id="projects" className="w-full">
-            <h2 className="after-content-[''] text-3xl font-semibold text-primary after:mt-1  after:block after:h-1 after:w-20 after:bg-primary">
-                Proyectos
-            </h2>
-            <div
-                className="my-10 grid justify-items-center gap-3"
-                style={{
-                    gridTemplateColumns: "repeat(auto-fit, minmax(280px, 1fr))",
-                    gridAutoRows: "minmax(200px, auto)",
-                }}
-            >
-                {projects.map((project) => {
-                    return (
-                        <figure
-                            className="card relative h-full w-full cursor-pointer bg-cover bg-center bg-no-repeat text-left text-base text-white transition duration-500 ease-in-out"
-                            key={project.id}
-                            style={{
-                                backgroundImage: `url(${pathImages(project.image)})`,
-                            }}
-                        >
-                            {/** title and description */}
-                            <figcaption className="z-1 absolute inset-0 flex flex-col justify-end bg-card p-2 transition duration-500 ease-in-out">
-                                <h3 className="text-lg opacity-100 transition duration-500 ease-in-out">
-                                    {project.title}
-                                </h3>
-                                <p className="text-sm opacity-100 transition duration-500 ease-in-out">
-                                    {project.description}
-                                </p>
-                            </figcaption>
-                            {/** buttons */}
-                            <figcaption className="z-2 absolute inset-0 hidden p-2 transition duration-500 ease-in-out">
-                                <button className="mb-2 w-24 rounded-full bg-primary p-2 text-sm text-white transition duration-500 ease-in-out hover:bg-secondary">
-                                    Ver Código
-                                </button>
-                                <button className="w-24 rounded-full bg-primary p-2 text-sm text-white transition duration-500 ease-in-out hover:bg-secondary">
-                                    Demo
-                                </button>
-                            </figcaption>
-                        </figure>
-                    );
-                })}
+            <h2 className="project-heading">Proyectos</h2>
+            <div className="project-grid">
+                {projects.map(({ id, title, description, image }) => (
+                    <figure
+                        className="project-card"
+                        key={id}
+                        style={{ backgroundImage: `url(${pathImages(image)})` }}
+                    >
+                        <figcaption className="project-content">
+                            <h3 className="project-title">{title}</h3>
+                            <p className="project-description">{description}</p>
+                        </figcaption>
+                        <figcaption className="project-buttons">
+                            <button className="project-code-button">Ver Código</button>
+                            <button className="project-demo-button">Demo</button>
+                        </figcaption>
+                    </figure>
+                ))}
             </div>
         </section>
     );
