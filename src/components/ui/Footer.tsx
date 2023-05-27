5da087ea647e6c574c23753a07e938683763a73b
d.saez <dssh27@gmail.com>
2023-05-27 16:15:15 -0400

    darken eliminado

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,8 +1,6 @@
-import { darken } from "@/helpers";
-
 export const Footer = () => {
     return (
-        <footer className="flex w-full flex-row items-center justify-center py-3 bg-darken">
+        <footer className="flex w-full flex-row items-center justify-center bg-darken py-3">
             <span className="text-sm font-bold text-white">Made with ❤️ by Daniel Sáez</span>
         </footer>
     );
